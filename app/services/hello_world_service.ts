828122db7cff3c35d32a23d1805bb409486c6b4b
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2025-02-06 11:54:07 +0100

    .

--- app/services/hello_world_service.ts
+++
@@ -1,9 +0,0 @@
-export class HelloWorldService {
-  public static add(a: number, b: number): number {
-    return a + b
-  }
-
-  public static remove(a: number, b: number): number {
-    return a + b
-  }
-}
--- tests/unit/unit/hello.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@japa/runner'
-import { HelloWorldService } from '#services/hello_world_service'
-
-test.group('Unit hello', () => {
-  test('example test', async ({ assert }) => {
-    assert.equal(HelloWorldService.add(1, 2), 3)
-  })
-})
