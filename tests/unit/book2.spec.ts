cf4c4db17798952a2b7524688df8ab50486b1d99
arthur.lecompte <arthur.lecompte14@gmail.com>
2025-02-06 11:59:09 +0100

    tests

--- tests/unit/book2.spec.ts
+++ tests/unit/book2.spec.ts
@@ -5,4 +5,8 @@ test.group('Book 2', () => {
   test('inutile', async ({ assert }) => {
     assert.equal(TddBookService.purchase(0, 0, 0, 0, 0), 0)
   })
+
+  test('inutile 2', async ({ assert }) => {
+    assert.equal(TddBookService.purchase(1, 0, 0, 0, 0), 0)
+  })
 })
