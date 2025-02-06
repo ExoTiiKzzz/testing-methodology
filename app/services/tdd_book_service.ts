6875739ce7ab01352d5158ef2245af187e83b60b
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2025-02-06 12:23:28 +0100

    .

--- app/services/tdd_book_service.ts
+++ app/services/tdd_book_service.ts
@@ -1,4 +1,13 @@
 export class TddBookService {
+  static unitPrice = 8
+  static coefficients: Record<number, number> = {
+    1: 1,
+    2: 0.95,
+    3: 0.9,
+    4: 0.8,
+    5: 0.75,
+  }
+
   static purchase(
     nbBook1: number,
     nbBook2: number,
@@ -6,30 +15,24 @@ export class TddBookService {
     nbBook4: number,
     nbBook5: number
   ): number {
-    let finalPrice = 0
-    let books = [nbBook1, nbBook2, nbBook3, nbBook4, nbBook5]
-    // eslint-disable-next-line @unicorn/no-for-loop
-    for (let i = 0; i < books.length; i++) {
-      if (books[i] > 1) {
-        finalPrice += 8
-        books[i]--
+    let price = 0
+    const volumes: Record<number, number> = {
+      1: nbBook1 || 0,
+      2: nbBook2 || 0,
+      3: nbBook3 || 0,
+      4: nbBook4 || 0,
+      5: nbBook5 || 0,
+    }
+    while (Object.values(volumes).reduce((acc, volume) => acc + volume, 0)) {
+      const differentVolumes = Object.values(volumes).filter((volume) => volume > 0).length
+      price += this.unitPrice * differentVolumes * this.coefficients[differentVolumes]
+      for (const key in volumes) {
+        if (volumes[key] > 0) {
+          volumes[key]--
+        }
       }
     }
-    if (books[0] === 1 && books[1] === 1 && books[2] === 1 && books[3] === 1 && books[4] === 1) {
-      return finalPrice + 40 * 0.75
-    }
-    if (books[0] === 1 && books[1] === 1 && books[2] === 1 && books[3] === 1) {
-      return finalPrice + 32 * 0.8
-    }
-    if (books[0] === 1 && books[1] === 1 && books[2] === 1) {
-      return finalPrice + 24 * 0.9
-    }
-    if (books[0] === 1 && books[1] === 1) {
-      return finalPrice + 16 * 0.95
-    }
-    if (books[0] === 1) {
-      return finalPrice + 8
-    }
-    return 0
+
+    return price
   }
 }
