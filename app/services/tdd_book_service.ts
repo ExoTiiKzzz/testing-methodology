e7da5218d5058b2bc90aef9aec9a06d55bdf6205
arthur.lecompte <arthur.lecompte14@gmail.com>
2025-02-06 11:57:08 +0100

    tests

---
+++ app/services/tdd_book_service.ts
@@ -0,0 +1,11 @@
+export class TddBookService {
+  static purchase(
+    nbBook1: number,
+    nbBook2: number,
+    nbBook3: number,
+    nbBook4: number,
+    nbBook5: number
+  ): number {
+    return 0
+  }
+}
---
+++ tests/unit/book2.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@japa/runner'
+import { TddBookService } from '#services/tdd_book_service'
+
+test.group('Book 2', () => {
+  test('inutile', async ({ assert }) => {
+    assert.equal(TddBookService.purchase(0, 0, 0, 0, 0), 0)
+  })
+})
