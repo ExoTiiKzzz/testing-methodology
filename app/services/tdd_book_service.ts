a4b473afeea627b7ef2007ada9917d05665fc034
Geoffrey <geoffrey160502@gmail.com>
2025-02-06 12:05:29 +0100

    code intile 3

--- app/services/tdd_book_service.ts
+++ app/services/tdd_book_service.ts
@@ -6,7 +6,10 @@ export class TddBookService {
     nbBook4: number,
     nbBook5: number
   ): number {
-    if(nbBook1 === 1 && nbBook2 === 1 && nbBook3 === 1) {
+    if (nbBook1 === 1 && nbBook2 === 1 && nbBook3 === 1 && nbBook4 === 1) {
+      return 32 * 0.8
+    }
+    if (nbBook1 === 1 && nbBook2 === 1 && nbBook3 === 1) {
       return 24 * 0.9
     }
     if (nbBook1 === 1 && nbBook2 === 1) {
